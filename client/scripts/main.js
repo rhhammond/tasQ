540fe84ccdd14ac2402b7d6b23112045fce1293f
Rob Hammond <robert.h.hammond@gmail.com>
2012-07-22 22:10:38 -0500

    Added require.js shim configuration
    Bootstrap.js and dropdown.js both require that jQuery be loaded first.
    Require.js lets you specify the order your dependencies are loaded via
    the shim section of the configuration.

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,4 +1,4 @@
-//require.js configuration - http://requirejs.org/docs/api.html#config
+// require.js configuration - http://requirejs.org/docs/api.html#config
 require.config({
 
 	baseUrl: 'scripts',
@@ -12,19 +12,26 @@ require.config({
 		dropdown: '../lib/bootstrap/js/bootstrap-dropdown',
 		//underscore: '../lib/underscore-amd',
 		//backbone: '../lib/backbone-amd'
+	},
+
+	// shim section specifies any dependencies that must be loaded before these modules can be loaded
+	// http://requirejs.org/docs/api.html#config-shim
+	shim: {
+		bootstrap: ['jquery'],
+		dropdown: ['bootstrap']
 	}
 
 });
 
-//use require.define() to declare module dependencies
+// use require.define() to declare module dependencies
 define([
 	'jquery',
-	//'bootstrap',
-	//'dropdown'
+	'bootstrap',
+	'dropdown'
 
 ], function($) {
 
 	//TODO: bootstrap the application here
 	$("#content").show();
-
+	$('.dropdown-toggle').dropdown()
 });
