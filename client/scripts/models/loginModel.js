1c1daf3268f5fc05a15877ab9c76b1ddd21b9266
Rob Hammond <robert.h.hammond@gmail.com>
2012-08-26 22:54:29 -0500

    created loginModel

---
+++ client/scripts/models/loginModel.js
@@ -0,0 +1,33 @@
+define([
+	'jquery',
+	'settings'
+], function($, settings) {
+
+	"use strict";
+
+	var LoginModel = Backbone.Model.extend({
+
+		urlRoot: settings.serverUrl,
+
+		//todo: specific auth route on server
+		//url:
+
+		login: function(username, password) {
+
+			var options = {
+				success: function(model, response) {
+					if(settings.rememberMe) {
+						// todo: save some auth information here
+					}
+				}
+			};
+
+			this.save({
+				username: username,
+				password: password
+			}, options);
+		}
+	});
+
+	return new LoginModel();
+});
